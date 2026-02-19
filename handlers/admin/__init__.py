b817f6c7cf724d5f956df980926dc58acc5bf3b2
dippelbiz <dippel.biz@gmail.com>
2026-02-20 00:29:30 +0700

    Update __init__.py

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -1,2 +1,9 @@
 # Пакет обработчиков администратора
+from .orders import admin_orders_conv
+from .payments import admin_payments_conv
+from .reports import admin_reports_conv
+from .settings import admin_settings_conv
 from .sellers import admin_sellers_handler
+from .backup import manual_backup
+from .restore import restore_conv
+from .add_test_seller import add_seller_handler  # ЭТО НУЖНО ДОБАВИТЬ
