8e923b2176f89f4915e93e948e40d3452085323a
dippelbiz <dippel.biz@gmail.com>
2026-02-20 15:01:01 +0700

    Update __init__.py

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -3,7 +3,7 @@ from .orders import admin_orders_conv
 from .payments import admin_payments_conv
 from .reports import admin_reports_conv
 from .settings import admin_settings_conv
-from .sellers import admin_sellers_handler
 from .backup import manual_backup
 from .restore import restore_conv
-from .add_test_seller import add_seller_handler  # ЭТО НУЖНО ДОБАВИТЬ
+from .add_test_seller import add_seller_handler
+# Удален импорт admin_sellers_handler
